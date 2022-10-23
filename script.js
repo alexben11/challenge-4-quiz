8d78966b0b7b0d5bde6a0e2fd2e13142361249d7
alexben11 <113950983+alexben11@users.noreply.github.com>
2022-10-23 11:18:28 -0400

    annotate HTML added all sections highscore,countdown, welcome msg, buttons, status

--- script.js
+++ script.js
@@ -1,27 +1,84 @@
-var student = document.getElementById("student-names");
-var grade = document.getElementById("grades");
-var comment = document.getElementById("msg");
-var saveButton = document.getElementById("save");
-var savedName = document.getElementById("saved-name");
 
-saveButton.addEventListener("click", function(event) {
-event.preventDefault();
-
-var studentGrade = {
-  student: student.value,
-  grade: grade.value,
-  comment: comment.value.trim()
-};
-
-localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
-renderMessage();
-
-});
-
-function renderMessage() {
-  var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
-  if (lastGrade !== null) {
-    document.querySelector(".message").textContent = lastGrade.student + 
-    " received a/an " + lastGrade.grade
-  }
+var timeToStart=60;
+function countDown() {
+    document.getElementById("countdown").innerText= "Time: " + timeToStart;
+    timeToStart--;
+    if (timeToStart=== -1) {
+      clearInterval(interval);
+      document.getElementById("countdown").remove();
+      var h5=document.createElement("h5");
+      h5.innerText="Time's up!";
+      document.body.appendChild(h1);
+    }
 }
+var interval=setInterval(countDown,1000);
+
+
+
+
+// var student = document.getElementById("student-names");
+// var grade = document.getElementById("grades");
+// var comment = document.getElementById("msg");
+// var saveButton = document.getElementById("save");
+// var savedName = document.getElementById("saved-name");
+
+// saveButton.addEventListener("click", function(event) {
+// event.preventDefault();
+
+// var studentGrade = {
+//   student: student.value,
+//   grade: grade.value,
+//   comment: comment.value.trim()
+// };
+
+// localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
+// renderMessage();
+
+// });
+
+// function renderMessage() {
+//   var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
+//   if (lastGrade !== null) {
+//     document.querySelector(".message").textContent = lastGrade.student + 
+//     " received a/an " + lastGrade.grade
+//   }
+// }
+
+
+// // Selects element by class
+// var timeEl = document.querySelector(".time");
+
+// // Selects element by id
+// var mainEl = document.getElementById("main");
+
+// var secondsLeft = 10;
+
+// function setTime() {
+//   // Sets interval in variable
+//   var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+//     if(secondsLeft === 0) {
+//       // Stops execution of action at set interval
+//       clearInterval(timerInterval);
+//       // Calls function to create and append image
+//       sendMessage();
+//     }
+
+//   }, 1000);
+// }
+
+// // Function to create and append colorsplosion image
+// function sendMessage() {
+//   timeEl.textContent = " ";
+//   var imgEl = document.createElement("img");
+//   imgEl.setAttribute("src", "images/image_1.jpg");
+//   mainEl.appendChild(imgEl);
+
+// }
+
+// setTime();
+
+
+
