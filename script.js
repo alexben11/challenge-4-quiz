db0267b8c2fcf69f872277ff40cb4a0e0a61d68b
alexben11 <113950983+alexben11@users.noreply.github.com>
2022-10-24 14:52:57 -0400

    trying to make this work

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+// start the game by pressing button
+var startButton = document.getElementById("start-btn");
+startButton.addEventListener('click',startGame);
+
+// show questions in container
+var questionContainerElement = document.getElementById ('question-container');
+
+
+function startGame () {
+console.log ('Started');
+//startButton.classList.add('hide');
+questionContainerElement.classList.remove('hide');
+setNextQuestion();
+}
+
+
+// select an answer
+
+function selextAnswer () {
+
+
+}
+
+
+
+// setting next question
+
+function setNextQuestion () {
+
+
+}
+
+//
+
 
 var timeToStart=60;
 function countDown() {
@@ -24,61 +58,3 @@ var interval=setInterval(countDown,1000);
 
 // saveButton.addEventListener("click", function(event) {
 // event.preventDefault();
-
-// var studentGrade = {
-//   student: student.value,
-//   grade: grade.value,
-//   comment: comment.value.trim()
-// };
-
-// localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
-// renderMessage();
-
-// });
-
-// function renderMessage() {
-//   var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
-//   if (lastGrade !== null) {
-//     document.querySelector(".message").textContent = lastGrade.student + 
-//     " received a/an " + lastGrade.grade
-//   }
-// }
-
-
-// // Selects element by class
-// var timeEl = document.querySelector(".time");
-
-// // Selects element by id
-// var mainEl = document.getElementById("main");
-
-// var secondsLeft = 10;
-
-// function setTime() {
-//   // Sets interval in variable
-//   var timerInterval = setInterval(function() {
-//     secondsLeft--;
-//     timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
-
-//     if(secondsLeft === 0) {
-//       // Stops execution of action at set interval
-//       clearInterval(timerInterval);
-//       // Calls function to create and append image
-//       sendMessage();
-//     }
-
-//   }, 1000);
-// }
-
-// // Function to create and append colorsplosion image
-// function sendMessage() {
-//   timeEl.textContent = " ";
-//   var imgEl = document.createElement("img");
-//   imgEl.setAttribute("src", "images/image_1.jpg");
-//   mainEl.appendChild(imgEl);
-
-// }
-
-// setTime();
-
-
-
